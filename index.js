41555cc0b2916508984ad1e39c2d9f603bde3490
jlukenoff <jlukenoff@gmail.com>
2019-02-07 12:50:51 -0800

    Add options for environment, locale, other params

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const request = require('request');
 const shell = require('shelljs');
 
 // get cli args
-const [moniker, env] = process.argv.slice(2);
+const [moniker, env, locale, params] = process.argv.slice(2);
 
 // function to extract retailerSetting from html string
 const getRetailerSettingsFromHtmlString = function(string) {
@@ -28,21 +28,37 @@ const getRetailerSettingsFromHtmlString = function(string) {
     .slice(0, -1);
 };
 
+const getDomain = function(env) {
+  const domains = [
+    'http://narvar.com/tracking',
+    'http://tracking-qa01.narvar.qa/tracking',
+    'http://tracking-st01.narvar.qa/tracking',
+  ];
+
+  if (!env || env.startsWith('prod')) {
+    return domains[0];
+  } else if (env === 'qa') {
+    return domains[1];
+  } else if (env.startsWith('stag')) {
+    return domains[2];
+  }
+};
+
 // render path to new settings file
 const path = `/Users/johnlukenoff/Desktop/Projects/${moniker}/config/RetailerSetting_${new Date()
   .toUTCString()
-  .replace(/\s/g, '')
-  .slice(0, -1)}.json`;
+  .replace(/[\s,:"GM"]/g, '')
+  .slice(0, -1)}${!env || env.startsWith('prod') ? '' : '-' + env}.json`;
 
 // make dirs
 shell.mkdir('-p', `/Users/johnlukenoff/Desktop/Projects/${moniker}/config`);
 
 console.log('Getting retailerSettings...');
 
-// TODO: accept environment input but default to prod
 // get current track page
 request(
-  `http://narvar.com/tracking/${moniker}/ups?trackingnumbers=test`,
+  `${getDomain(env)}/${moniker}/ups?trackingnumbers=test&locale=${locale ||
+    'en_US'}${params ? '&' + params : ''}`,
   { encoding: null },
   function(err, res, body) {
     if (err) console.error(err);
